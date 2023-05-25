186a5c8d85463924d0a3f89d2b5e628c6b0af675
akdhiman358 <86318224+akdhiman358@users.noreply.github.com>
2023-05-25 16:01:05 +0530

    Update Card.jsx
    fixed issue with the link to google maps

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ export default function Card(props){
         <div className="card">
            <img src={props.item.imageUrl} alt="" className="card-img"/>
         <div className="card-text">
-           <div className="location"><img src="/images/location.png" alt="" className="location-img" /> <span className="location-country">{props.item.location}</span><a href="{props.item.googleMapsUrl}" className="location-maps">view on google maps</a></div>
+           <div className="location"><img src="/images/location.png" alt="" className="location-img" /> <span className="location-country">{props.item.location}</span><a href={props.item.googleMapsUrl} className="location-maps">view on google maps</a></div>
            <div className="card-text-heading">{props.item.title}</div>
            <div className="card-text-date">{props.item.startDate} - {props.item.endDate}</div>
         <div className="card-description">{props.item.description}</div>
